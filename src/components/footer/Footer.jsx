1ab8d456e5edca6e119c578046c80912558038a9
The-Unknown-077 <gamergamervich@gmail.com>
2025-07-05 19:30:01 +0500

    New

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Footer.css";
+import logo from "../../assets/Escape..svg";
+import { FOOTER_LINKS } from "../../static/static";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        <img src={logo} alt="Footer Logo" className="footer_logo" />
+        <ul className="footer_collection">
+          {FOOTER_LINKS?.map((item, index) => (
+            <li className="footer_item" key={index}>
+              <a href={item.href}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
